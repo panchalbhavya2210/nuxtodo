152f8c2de0e570cf0104f5c100b366f4620568e9
panchalbhavya2210 <panchalbhavya2210@gmail.com>
2023-12-16 21:11:04 +0530

    getting data done

--- pages/index.vue
+++ pages/index.vue
@@ -4,23 +4,46 @@ const route = useRoute()
 
 const supbase = useSupabaseClient();
 
-let emailVal = "panchalbhavya2210@gmail.com"
-let passwordVal = "123456"
+/**
+ * * this is a function of signup
 
-const handleSignUp = async () => {
-  try {
-    const { error } = await supbase.auth.signUp({ email: emailVal, password: passwordVal })
-    console.log(error)
-  }
-  catch {
 
-  }
-}
+// const handleSignUp = async () => {
+//   try {
+//     const { error } = await supbase.auth.signUp({ email: emailVal, password: passwordVal })
+//     console.log(error)
+//   }
+//   catch {
+
+//   }
+// }
+ */
+
+// let emailVal = "panchalbhavya2210@gmail.com"
+// let passwordVal = "123456"
+
+// const handleLogin = async () => {
+//   try {
+//     const { error } = await supbase.auth.signInWithPassword({ email: emailVal, password: passwordVal })
+//   }
+//   catch {
+
+//   }
+
+// }
+
+// handleLogin()
+
+
+const res = await supbase.auth.getUser()
+//@ts-ignore
+const uid = res.data.user.id
+console.log(res)
+console.log(uid)
 </script>
 
 <template>
   <div class="todoRender">
-
     hello from index
   </div>
 </template>
